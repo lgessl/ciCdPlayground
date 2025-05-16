24d1e09a43290bd219065e2c93dc5afd5252a738
Lukas Gessl (TNG) <lg@ip-192-168-178-46.eu-central-1.compute.internal>
2025-05-16 13:42:39 +0200

    personal notes in website

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Lukas",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Test on multiple platform", "already-existent pipelines"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: ["GitHub Actions", "GitLab CI", "Jenkins"],
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["More foundational understanding of CI/CD"],
   },
 });
